5e0c41e4aa565ef576a972a50d703ffa41a15a3e
victorjose2389-sketch <victorjose2389@gmail.com>
2026-05-18 17:56:44 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+// Service Worker que se auto-destruye y limpia todo el caché
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.registration.unregister())
+  );
+});
